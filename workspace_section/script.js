f19315774eae0b74ad44ecd325a761924a0fa5aa
Gooblicraft <24crafter05@gmail.com>
2026-01-26 22:41:19 +0800

    Fix block options display and images
    - Fix display of the block options
    - Fix the class naming
    - Added a hover effect
    - Changes of the images respective to each block options
    - Temporarily remove the open modal with the window alert for debugging

--- workspace_section/script.js
+++ workspace_section/script.js
@@ -1,12 +1,12 @@
 const blockOptions = [
     { type: 'plain', label: 'Plain', img:'images/cards/plain_block.png'},
-    { type: 'rotateble', label: 'Rotateble', img:'images/cards/plain_block.png'},
-    { type: 'changeable', label: 'Changeable', img:'images/cards/plain_block.png'},
-    { type: '1by2_block', label: '1by2 Block', img:'images/cards/plain_block.png'},
-    { type: '2by1_block', label: '2by1 Block', img:'images/cards/plain_block.png'},
-    { type: 'vertical', label: 'Vertical', img:'images/cards/plain_block.png'},
-    { type: 'horizontal', label: 'Horizontal', img:'images/cards/plain_block.png'},
-    { type: 'door', label: 'Door', img:'images/cards/plain_block.png'}
+    { type: 'rotateble', label: 'Rotateble', img:'images/cards/rotateble_block.png'},
+    { type: 'changeable', label: 'Changeable', img:'images/cards/changeable_block.png'},
+    { type: '1by2_block', label: '1by2 Block', img:'images/cards/1by2_block.png'},
+    { type: '2by1_block', label: '2by1 Block', img:'images/cards/2by1_block.png'},
+    { type: 'vertical', label: 'Vertical', img:'images/cards/vertical_block.png'},
+    { type: 'horizontal', label: 'Horizontal', img:'images/cards/horizontal_block.png'},
+    { type: 'door', label: 'Door', img:'images/cards/door_block.png'}
 ]
 
 const container = document.getElementById('make_block_option');
@@ -16,7 +16,7 @@ blockOptions.forEach(option =>{
     block_option.className='block_card';
     
     block_option.onclick = function() {
-        openModal(option.type);
+        window.alert("You Clicked it!")
     };
 
     block_option.innerHTML = `
