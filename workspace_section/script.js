1a2d95aa75d503854db4c1cc86e882047ed0583a
Gooblicraft <24crafter05@gmail.com>
2026-01-30 19:09:56 +0800

    update the option_block from separate divs into a button type

--- workspace_section/script.js
+++ workspace_section/script.js
@@ -1,23 +1,24 @@
 const blockOptions = [
-    { type: 'plain', label: 'Plain', img:'images/cards/plain_block.png'},
-    { type: 'rotateble', label: 'Rotateble', img:'images/cards/rotateble_block.png'},
-    { type: 'changeable', label: 'Changeable', img:'images/cards/changeable_block.png'},
-    { type: '1by2_block', label: '1by2 Block', img:'images/cards/1by2_block.png'},
-    { type: '2by1_block', label: '2by1 Block', img:'images/cards/2by1_block.png'},
-    { type: 'vertical', label: 'Vertical', img:'images/cards/vertical_block.png'},
-    { type: 'horizontal', label: 'Horizontal', img:'images/cards/horizontal_block.png'},
-    { type: 'door', label: 'Door', img:'images/cards/door_block.png'}
+    { id:1, type: 'plain', label: 'Plain', img:'images/cards/plain_block.png'},
+    { id:2, type: 'rotateble', label: 'Rotateble', img:'images/cards/rotateble_block.png'},
+    { id:3, type: 'changeable', label: 'Changeable', img:'images/cards/changeable_block.png'},
+    { id:4, type: '1by2_block', label: '1by2 Block', img:'images/cards/1by2_block.png'},
+    { id:5, type: '2by1_block', label: '2by1 Block', img:'images/cards/2by1_block.png'},
+    { id:6, type: 'vertical', label: 'Vertical', img:'images/cards/vertical_block.png'},
+    { id:7, type: 'horizontal', label: 'Horizontal', img:'images/cards/horizontal_block.png'},
+    { id:8, type: 'door', label: 'Door', img:'images/cards/door_block.png'}
 ]
 
 const container = document.getElementById('make_block_option');
 
+function clickBlockOption(id){
+    alert(`Clicked ${blockOptions[id-1].label}`);
+}
+
 blockOptions.forEach(option =>{
-    const block_option = document.createElement('div');
+    const block_option = document.createElement('button');
     block_option.className='block_card';
-    
-    block_option.onclick = function() {
-        window.alert("You Clicked it!")
-    };
+    block_option.onclick= () => clickBlockOption(option.id);
 
     block_option.innerHTML = `
         <img src="${option.img}" alt="${option.label}">
